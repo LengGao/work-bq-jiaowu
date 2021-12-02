bed5324988c057997f901c3f09d5f358aab49945
qinpengcheng <754530433@qq.com>
2021-12-02 11:57:54 +0800

    fix:修改工作台跳转

--- src/views/pms/components/DataStatistics/components/SalesData.vue
+++ src/views/pms/components/DataStatistics/components/SalesData.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="sales-data">
-    <div class="sales-data-item">
+    <div class="sales-data-item" @click="$emit('item-click', 1)">
       <p class="sales-data-item-title">招生客户</p>
       <p class="sales-data-item-value">{{ data.customerNum || 0 }}</p>
     </div>
-    <div class="sales-data-item">
+    <div class="sales-data-item" @click="$emit('item-click', 2)">
       <p class="sales-data-item-title">招生订单</p>
       <p class="sales-data-item-value">{{ data.orderNum || 0 }}</p>
     </div>
-    <div class="sales-data-item">
+    <div class="sales-data-item" @click="$emit('item-click', 3)">
       <p class="sales-data-item-title">渠道学生</p>
       <p class="sales-data-item-value">{{ data.channelCustomers || 0 }}</p>
     </div>
-    <div class="sales-data-item">
+    <div class="sales-data-item" @click="$emit('item-click', 4)">
       <p class="sales-data-item-title">机构回款</p>
       <p class="sales-data-item-value">{{ data.channelOrder || 0 }}</p>
     </div>
-    <div class="sales-data-item">
+    <div class="sales-data-item" @click="$emit('item-click', 5)">
       <p class="sales-data-item-title">订单金额</p>
       <p class="sales-data-item-value">
         {{ unitFormat(data.orderMoney)
@@ -67,6 +67,7 @@ export default {
     &:not(:last-child) {
       border-right: 1px solid #f4f5f7;
     }
+
     &-title {
       font-size: 14px;
       color: #666;
@@ -75,8 +76,17 @@ export default {
     &-value {
       font-size: 36px;
       color: #666;
+
       span.unit {
         font-size: 14px;
+      }
+    }
+    &:nth-child(-n + 3) {
+      cursor: pointer;
+      &:hover {
+        .sales-data-item-value {
+          color: #199fff;
+        }
       }
     }
   }
