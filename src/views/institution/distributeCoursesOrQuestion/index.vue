15c37d66fb899a741c7e5be325de63cf9e09a13b
qinpengcheng <754530433@qq.com>
2022-01-02 14:11:29 +0800

    fix:修改机构菜单

--- src/views/institution/distributeCoursesOrQuestion/index.vue
+++ src/views/institution/distributeCoursesOrQuestion/index.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="distribute">
-    <div class="head_remind">*将项目/院校专业批量分发给入驻机构</div>
+    <div class="head_remind">*将课程/题库批量分发给入驻机构</div>
     <div class="container">
       <el-tabs v-model="activeName">
-        <el-tab-pane label="分发项目" name="distributeProject"></el-tab-pane>
+        <el-tab-pane label="分发课程" name="distributeCourses"></el-tab-pane>
         <el-tab-pane
-          label="分发院校专业"
-          name="distributeEducation"
+          label="分发题库"
+          name="distributeQuestionBak"
         ></el-tab-pane>
-        <el-tab-pane label="班型管理" name="classType"></el-tab-pane>
       </el-tabs>
       <component :is="getComponent" />
     </div>
@@ -20,7 +19,7 @@ export default {
   name: "distribute",
   data() {
     return {
-      activeName: "distributeProject",
+      activeName: "distributeCourses",
     };
   },
   computed: {
