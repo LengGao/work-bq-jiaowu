9b54b8ac9b1674c1e9bcd9b308f29c95e6535b73
qinpengcheng <754530433@qq.com>
2022-02-26 13:46:46 +0800

    fix:修改图片查看

--- src/cropper.js
+++ src/cropper.js
@@ -3,7 +3,5 @@ import Viewering from 'v-viewer' // 预览相关
 import 'viewerjs/dist/viewer.css' // 预览相关
 Viewering.setDefaults({
   zIndex: 9999,
-  navbar: false,
-  toolbar: false,
 })
 Vue.use(Viewering)
